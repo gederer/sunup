69d01d5fbc6364cf5c7aac39e22daa062568fc03
Greg Ederer <greg@makereal.com>
2025-10-28 15:52:59 -0700

    cleanup home page

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,4 +1,14 @@
-import { defineSchema } from "convex/server";
+import {defineSchema, defineTable} from "convex/server";
+import { v } from "convex/values";
 
 // Extend this schema with tables as your data model evolves.
-export default defineSchema({});
+export default defineSchema({
+  numbers: defineTable({
+    value: v.number(),
+  }),
+  users: defineTable({
+    name: v.string(),
+    // this the Clerk ID, stored in the subject JWT field
+    externalId: v.string(),
+  }).index("byExternalId", ["externalId"]),
+});
